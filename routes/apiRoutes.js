77e27bf6e0f99d97fd427b3e363c3a271789153e
Michael W Lee <mikewclee@gmail.com>
2020-09-08 18:10:38 -0400

    heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,40 +2,42 @@ var db = require("../models");
 
 module.exports = function (app) {
 
-  app.post("/api/workouts", (req, res) => {
-    db.Workout.create({}).then(dbExercise => {
-      res.json(dbExercise);
+    app.post("/api/workouts", ({ body }, res) => {
+        db.Workout.create(body)
+            .then(dbExercise => {
+                res.json(dbExercise);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => res.json(err));
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        console.log(req.params.id);
+        db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } }, { new: true })
+            .then((dbWorkout) => {
+                res.json(dbWorkout);
+            })
+            .catch(err => res.json(err));
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
+            .limit(7) // { day: 7} )
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-
-  app.get("/api/workouts", function (req, res) {
-    db.Workout.find({}).then( dbWorkout => {
-      res.json(dbWorkout);
-    })
-      .catch(err => res.json(err));
-  });
-
-  app.put("/api/workouts/:id", function (req, res) {
-    console.log(req.body);
-    console.log(req.params.id);
-    db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body } }, { new: true })
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      });
-  });
-
-  app.get("/api/workouts/range", (req, res) => {
-    console.log("range");
-    db.Workout.find().limit(7)
-      .then(dbworkout => {
-        res.json(dbworkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  })
 
 };
