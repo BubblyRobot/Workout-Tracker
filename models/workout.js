c62a3ceef4a08dae021df6ddb24244668d81da29
Michael W Lee <mikewclee@gmail.com>
2020-09-08 23:21:18 -0400

    model change

--- models/workout.js
+++ models/workout.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const workoutSchema = new Schema({
   day: {
     type: Date,
-    default: () => new Date().setDate(new Date().getDate())
+    default: Date.now
   },
   exercises: [
     {
